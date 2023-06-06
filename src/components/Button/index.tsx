aa8b389c449ddc3dcae1cc22a1e7fd94aba320ed
Daniel Kyochi <michigamidaniel77@gmail.com>
2023-06-06 19:18:15 -0400

    initial commit, ignite timer

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { ButtonVariant, Container } from "./styles"
+
+interface ButtonProps {
+  variant?: ButtonVariant
+}
+
+export function Button({ variant = 'primary' }: ButtonProps) {
+  return (
+    <Container variant={variant}>Enviar</Container>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'sucess'
+
+interface ButtonContainerProps {
+    variant: ButtonVariant
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secondary: 'orange',
+    danger: 'red',
+    sucess: 'green'
+}
+
+export const Container = styled.button<ButtonContainerProps>`
+    width: 100px;
+    height: 40px;
+
+    background-color: ${props => props.theme.primary};
+/* ${props => {
+    return `background-color: ${buttonVariants[props.variant]}`
+}} */
+`
