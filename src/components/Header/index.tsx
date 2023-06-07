182d3c168eb2ef091e25da4e78de2771ff89ef1b
Daniel Kyochi <michigamidaniel77@gmail.com>
2023-06-07 16:40:24 -0400

    add new files

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,22 @@
+import { Timer, Scroll } from '@phosphor-icons/react'
+
+import igniteLogo from '../../assets/logo-ignite.svg'
+
+import { HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+
 export default function Header() {
-  return <div>HEADER</div>
+  return (
+    <HeaderContainer>
+      <img src={igniteLogo} alt="" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
