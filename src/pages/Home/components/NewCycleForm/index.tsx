a87e4b76e3b01304234547658988df15e9278ca6
Daniel Kyochi <michigamidaniel77@gmail.com>
2023-06-15 17:03:42 -0400

    add new files

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,35 @@
+import { FormContainer, TaskInput, MinutesAmountInput } from './styles'
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Estudar para a faculdade" />
+        <option value="Lavar a casa" />
+        <option value="Ler livros" />
+      </datalist>
+
+      <label htmlFor="">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
